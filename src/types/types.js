d144f0aab19936d65113b46048bf6d0f18a142c5
Jesus Delgado <m6nuel@gmail.com>
2021-10-14 13:41:46 -0300

    first commit

---
+++ src/types/types.js
@@ -0,0 +1,27 @@
+
+export const types = {
+
+    uiOpenModal: '[ui] Open modal',
+    uiCloseModal: '[ui] Close modal',
+
+
+    eventSetActive: '[event] Set Active',
+    eventLogout: '[event] Logout event',
+
+
+    eventStartAddNew: '[event] Start add new',
+    eventAddNew: '[event] Add new',
+    eventClearActiveEvent: '[event] Clear active event',
+    eventUpdated: '[event] Event updated',
+    eventDeleted: '[event] Event deleted',
+    eventLoaded: '[event] Events loaded',
+
+
+    authChekingFinish: '[auth] Finish checking login state',
+    authStartLogin: '[auth] Start login ',
+    authLogin: '[auth] login ',
+    authStartRegister: '[auth] Start Register',
+    authStartTokenRenew: '[auth] Start token renew',
+    authLogout: '[auth] Logout',
+
+}
