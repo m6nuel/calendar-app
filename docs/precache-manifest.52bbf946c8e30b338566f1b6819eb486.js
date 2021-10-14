e801e7dd8df30c165ee94573c6793d58de569cf5
Jesus Delgado <m6nuel@gmail.com>
2021-10-14 13:50:15 -0300

    archivo docs

---
+++ docs/precache-manifest.52bbf946c8e30b338566f1b6819eb486.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e97966879130fd0ed6e2a9a368f29fab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2be79f90bcc8d65d7506",
+    "url": "/static/css/2.59a0ab78.chunk.css"
+  },
+  {
+    "revision": "d7028094789c71c0a817",
+    "url": "/static/css/main.6020f20c.chunk.css"
+  },
+  {
+    "revision": "2be79f90bcc8d65d7506",
+    "url": "/static/js/2.bd64b146.chunk.js"
+  },
+  {
+    "revision": "fa4b4056bba234820139274405351863",
+    "url": "/static/js/2.bd64b146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7028094789c71c0a817",
+    "url": "/static/js/main.75f3966c.chunk.js"
+  },
+  {
+    "revision": "c2ab9c45931d577cf16e",
+    "url": "/static/js/runtime-main.27b01e2e.js"
+  }
+]);
